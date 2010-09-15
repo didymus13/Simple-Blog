fe14b858b4a539f6e293bb4a59e3f59a933cefea
Stephane D <maddox@scooter.muppet.show>
2010-09-14 21:41:13 -0400

    tag leftovers

--- admin.py
+++ admin.py
@@ -4,8 +4,4 @@ from django.contrib import admin
 class EntryAdmin(admin.ModelAdmin):
 	prepopulated_fields = {"slug": ("title",) }
 
-class TagAdmin(admin.ModelAdmin):
-	prepopulated_fields = {"slug": ("name",) }
-
 admin.site.register(Entry, EntryAdmin )
-admin.site.register(Tag, TagAdmin )
